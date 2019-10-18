25addddeb0a7c57536614d8f28c049e848ab86dd
alumno <alumno@LAB00PC00>
2019-10-17 22:26:57 -0300

    Agrego D E F G

--- DorsoJuanMartin1erParcial/Debug/src/DorsoJuanMartin1erParcial.d
+++ DorsoJuanMartin1erParcial/Debug/src/DorsoJuanMartin1erParcial.d
@@ -1,8 +1,10 @@
 src/DorsoJuanMartin1erParcial.o: ../src/DorsoJuanMartin1erParcial.c \
- ../src/utn.h ../src/cliente.h ../src/pedidos.h
+ ../src/utn.h ../src/cliente.h ../src/pedidos.h ../src/informes.h
 
 ../src/utn.h:
 
 ../src/cliente.h:
 
 ../src/pedidos.h:
+
+../src/informes.h:
